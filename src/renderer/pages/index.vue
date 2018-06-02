19521b1b94c187bd60706d60254ec70c0499a806
xmflswood <774898896@qq.com>
2018-06-02 15:36:38 +0800

    test for save local

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <el-button>{{test}}</el-button>
+    <el-button @click="save">{{test}}</el-button>
   </div>
 </template>
 
 <script>
+  import { remote } from 'electron'
+  const fs = require('fs')
+  const l = require('path').resolve(remote.app.getAppPath(), 'db.txt')
   export default {
     data () {
       return {
         test: '123'
+      }
+    },
+    methods: {
+      save () {
+        fs.writeFileSync(l, '2s')
       }
     }
   }
